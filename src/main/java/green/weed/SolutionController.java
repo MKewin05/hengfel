07a49f8a73b777e07c9548c4c76bc1a47385258b
MKewin05 <markokevin@ktch.hu>
2025-04-13 16:24:20 +0200

    Fajlba_iras

--- src/main/java/green/weed/SolutionController.java
+++ src/main/java/green/weed/SolutionController.java
@@ -1,4 +1,3 @@
-
 /*
 * File: SoulutionController.java
 * Author: Markó Kevin
@@ -15,6 +14,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
 public class SolutionController {
 
     @FXML
@@ -28,16 +32,31 @@ public class SolutionController {
 
     @FXML
     void onClickCalcButton(ActionEvent event) {
-        String l = lenghtField.getText();
-        double lenght = Double.parseDouble(l);
-        String r = radiusField.getText();
-        double radius = Double.parseDouble(r);
+        try {
+            String l = lenghtField.getText();
+            double lenght = Double.parseDouble(l);
+            String r = radiusField.getText();
+            double radius = Double.parseDouble(r);
 
-        Double result = calcResult(radius, lenght);
-        this.resultField.setText(result.toString());
+            Double result = calcResult(radius, lenght);
+            this.resultField.setText(result.toString());
+
+            // Eredmény fájlba írása
+            writeResultToFile(radius, lenght, result);
+        } catch (NumberFormatException e) {
+            resultField.setText("Hibás bemenet!");
+        } catch (IOException e) {
+            resultField.setText("Fájl írási hiba!");
+        }
     }
 
     public double calcResult(double radius, double length) {
         return 2 * Math.PI * radius * (radius + length);
+    }
+
+    private void writeResultToFile(double radius, double length, double result) throws IOException {
+        String output = String.format("Sugár: %.2f cm, Hossz: %.2f cm, Eredmény: %.2f cm2%n", radius, length, result);
+        Path filePath = Path.of("results.txt");
+        Files.writeString(filePath, output, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
     }
 }
